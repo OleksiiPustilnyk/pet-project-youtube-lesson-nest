637633b00970a72af5346a822ff4c80a3f106744
Oleksii Pustilnyk <pustilnyk.oleksii@gmail.com>
2024-05-14 15:19:57 +0300

    Добавляем Jwt strategy в наш проэкт на Nest js Используем useGuards для авторизации

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDTO } from '../users/dto';
 import { UserLoginDTO } from './dto';
 import { AuthUserResponse } from './response';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/guards/jwt-guard';
 
 @Controller('auth')
 export class AuthController {
@@ -11,6 +12,7 @@ export class AuthController {
 
     @ApiTags('API')
     @ApiResponse({ status: 201, type: CreateUserDTO })
+    @HttpCode(201)
     @Post('register')
     register(@Body() dto: CreateUserDTO): Promise<CreateUserDTO> {
         return this.authService.registerUsers(dto);
@@ -18,8 +20,15 @@ export class AuthController {
 
     @ApiTags('API')
     @ApiResponse({ status: 200, type: AuthUserResponse })
+    @HttpCode(200)
     @Post('login')
     login(@Body() dto: UserLoginDTO): Promise<AuthUserResponse> {
         return this.authService.loginUser(dto);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Post('test')
+    test() {
+        return true;
     }
 }
