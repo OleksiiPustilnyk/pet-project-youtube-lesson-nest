528e08e9e2e9afbdbf8894dfb3b51bd0f60fb629
Oleksii Pustilnyk <pustilnyk.oleksii@gmail.com>
2024-05-14 11:28:15 +0300

    Добавляем типизацию в User service Nest js Реализовываем уникальность email пользователя

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from './models/user.model';
 import * as bcrypt from 'bcrypt';
 import { CreateUserDTO } from './dto';
+import { AppError } from 'src/common/errors';
 
 @Injectable()
 export class UserService {
@@ -14,9 +15,34 @@ export class UserService {
         return bcrypt.hash(password, 10);
     }
 
+    async findUserByEmail(email: string) {
+        return this.userRepository.findOne({ where: { email } });
+    }
+
     async createUser(dto): Promise<CreateUserDTO> {
+        //если юзер уже есть с таким email
+        const existUser = await this.findUserByEmail(dto.email);
+        if (existUser) throw new BadRequestException(AppError.USER_EXIST);
+        //
+
         dto.password = await this.hashPassword(dto.password);
-        await this.userRepository.create(dto);
+
+        // 1 способ создания юзера
+        // const newUser = {
+        //     firstName: dto.firstName,
+        //     username: dto.username,
+        //     email: dto.email,
+        //     password: dto.password,
+        // };
+        // await this.userRepository.create(newUser);
+
+        // 2 способ создания юзера
+        await this.userRepository.create({
+            firstName: dto.firstName,
+            username: dto.username,
+            email: dto.email,
+            password: dto.password,
+        });
         return dto;
     }
 }
